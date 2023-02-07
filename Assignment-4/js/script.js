908e8d9806d18d0049c2a1152f019b66b7f9e9b7
anpshty <59507292+anpshty@users.noreply.github.com>
2023-02-07 10:09:09 +0530

    Update script.js

--- Assignment-4/js/script.js
+++ Assignment-4/js/script.js
@@ -1,1 +1,16 @@
+(function () {
 
+    var names = ["Yaakov", "Johnathan", "Jenson", "Peter", "Frank", "Larry", "Paula", "Jimmy", "Linda"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        goodbye.print(names[i]);
+      } 
+      else {
+        hello.print(names[i]);
+      }
+    }
+    
+    })();
