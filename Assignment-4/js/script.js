bb1755e8a4e1e73f06a4635df999c2f2b1e6100b
anpshty <59507292+anpshty@users.noreply.github.com>
2023-02-07 10:32:59 +0530

    Update script.js

--- Assignment-4/js/script.js
+++ Assignment-4/js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "Johnathan", "Jenson", "Peter", "Frank", "Larry", "Paula", "Jimmy", "Linda"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
