b40d522a4859b411e34c52a9e4b6a384840db12d
anpshty <59507292+anpshty@users.noreply.github.com>
2023-02-07 10:12:46 +0530

    Update goodbye.js

--- Assignment-4/js/goodbye.js
+++ Assignment-4/js/goodbye.js
@@ -1,1 +1,10 @@
-
+(function (window){
+    var goodbyePrinter = {};
+    var printWord = "Good Bye";
+    
+    goodbyePrinter.print = function (names) {
+      console.log(printWord + " " + names);
+    }
+     
+     window.goodbyePrinter = goodbyePrinter;
+    })(window);
